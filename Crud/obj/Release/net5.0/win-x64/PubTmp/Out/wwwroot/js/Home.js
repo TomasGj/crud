50933e010522d30ae58265a5c6fa077482eb16fd
TomasGj <bimagalicia@gmail.com>
2021-11-06 12:55:31 -0600

    Primer proyecto

---
+++ Crud/obj/Release/net5.0/win-x64/PubTmp/Out/wwwroot/js/Home.js
@@ -0,0 +1,8 @@
+﻿jQuery(document).ready(function (e) {
+	jQuery('.vertical-slider').flexslider({
+		animation: "slide",
+		direction: "vertical",
+		controlNav: false,
+		directionNav: true
+	});
+});
