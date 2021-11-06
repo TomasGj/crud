3ce4351fbc6f075d98b825818c82b15b69fbec78
TomasGj <bimagalicia@gmail.com>
2021-11-05 21:33:26 -0600

    Agregar archivos de proyecto.

---
+++ Crud/Datos/conexionBD.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Crud.Models;
+
+namespace Crud.Datos
+{
+    public class conexionBD:DbContext
+    {
+
+        public conexionBD(DbContextOptions<conexionBD> options):base(options)
+        {
+
+        }
+        public DbSet<cita> citas { get; set; }
+
+    }
+}
